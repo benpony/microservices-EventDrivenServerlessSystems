39e9f268c615a5b0000d748fc336b33107904f37
ben pony <ben.pony.dev@gmail.com>
2020-10-22 23:23:15 +0300

    faas callout mongodb integration

--- openfaas/words-counter-batch.py
+++ openfaas/words-counter-batch.py
@@ -1,18 +1,36 @@
 from datetime import datetime, timedelta
-from dateutil.tz import tzutc, UTC
+from dateutil.tz import tzutc
 import boto3
+import requests
+import pymongo
 
 REGION = "eu-west-3"
 BUCKET_NAME = "bpserverlessdemo"
-
 s3 = boto3.resource(
     's3',
-    region_name = REGION
+    region_name=REGION
 )
-bucket = s3.Bucket(BUCKET_NAME)
 
-for object in bucket.objects.all():
-    if object.last_modified > datetime.now(tzutc()) - timedelta(hours = 24):
-        # <download the file>
+TEMP_PASS = "admin"
+DB_NAME = "serverlessdemodb"
+client = pymongo.MongoClient(
+    f"mongodb+srv://admin:{TEMP_PASS}@cluster0.14ono.mongodb.net/{DB_NAME}?retryWrites=true&w=majority"
+)
+
+for s3object in s3.Bucket(BUCKET_NAME).objects.all():
+    # TODO change to '>' before creating cron
+    if s3object.last_modified < datetime.now(tzutc()) - timedelta(hours=24):
+        # <download the file> ?? --> bucket.download_file(s3object.key, s3object.key)
         # <send it to words counter in openfaas>
         # <save to database>
+        response = requests.post(
+            'http://127.0.0.1:8080/function/word-counter.openfaas-fn',
+            data=s3object.get()['Body'].read()
+        )
+        numOfWords = response.json()
+        db = client.serverlessdemodb
+        db.Words.insert_one({
+            "ObjectPath": f'''{s3object.bucket_name}/{s3object.key}''',
+            "Date": datetime.now(),
+            "AmountOfWords": numOfWords
+        })
