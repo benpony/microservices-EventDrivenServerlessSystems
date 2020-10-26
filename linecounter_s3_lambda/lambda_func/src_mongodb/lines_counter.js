7111760e4b928d67565e70be68124092945cfa6a
ben pony <ben.pony.dev@gmail.com>
2020-10-26 14:27:33 +0200

    nodejs mongo based aws lambda func - impl

--- linecounter_s3_lambda/lambda_func/src_mongodb/lines_counter.js
+++ linecounter_s3_lambda/lambda_func/src_mongodb/lines_counter.js
@@ -2,69 +2,45 @@
 const AWS = require('aws-sdk');
 const s3 = new AWS.S3();
 const { v4: uuidv4 } = require('uuid');
-const MONGODB_URI = process.env.MONGODB_URI; // or Atlas connection string
 
-let cachedDb = null;
 
-function connectToDatabase(uri) {
-    console.log(`=> connect to database`);
-    if (cachedDb) {
-        console.log(`=> using cached database instance`);
-        return Promise.resolve(cachedDb);
+`
+curl --header "X-Vault-Token: s.A946lZFDo7WOrX9v39d5Gf6h" \
+     --request GET 'http://127.0.0.1:8200/v1/kv-v1/db'
+`
 
-    }
-    
-    return MongoClient.connect(uri)
-        .then(db => {
-            cachedDb = db;
-            return cachedDb;
-        });
-}
+const DB_PASS = result['data']['pass']
+const DB_NAME = result['data']['dbname']
+const MongoClient = require('mongodb').MongoClient;
+const uri = `mongodb+srv://admin:${DB_PASS}@cluster0.14ono.mongodb.net/${DB_NAME}?retryWrites=true&w=majority`;
+const client = new MongoClient(uri, { useNewUrlParser: true });
 
-function queryDatabase(db) {
-    console.log('=> query database');
-
-    return db.collection('items').find({}).toArray()
-        .then(() => {
-            return { statusCode: 200, body: 'success' };
-        })
-        .catch(err => {
-            console.log('=> an error occurred: ', err);
-            return { statusCode: 500, body: 'error' };
-        });
-}
 
 exports.handler = async (event, context, callback) => {
     context.callbackWaitsForEmptyEventLoop = false;
     console.log('event: ', event);
     
     const bucket = event.Records[0].s3.bucket.name;
-    const key = event.Records[0].s3.object.key;]
+    const key = event.Records[0].s3.object.key;
     const fileObject = await s3.getObject({
         Bucket: bucket,
         Key: key
     }).promise();
     
-    const getObjectResult = await s3.getObject(params).promise() // await the promise
-    const fileContent = getObjectResult.Body.toString('utf-8');
+    const getObjectResult = await s3.getObject(params).promise()
+    const fileContent = getObjectResult['Body'].toString('utf-8');
     
-    connectToDatabase(MONGODB_URI)
-        .then(async db => {
-            await db.insertRow({
-                Id: uuidv4(),
-                ObjectPath: `${bucket}/${key}`,
-                Date: new Date(),
-                AmountOfLines: fileContent.split("\n")
-            })
-            return queryDatabase(db)
+    client.connect(async err => {
+        const collection = client.db("test").collection("Words");
+        await db.insertRow({
+            Id: uuidv4(),
+            ObjectPath: `${bucket}/${key}`,
+            Date: new Date(),
+            AmountOfLines: fileContent.split("\n")
         })
-        .then(result => {
-            console.log('=> returning result: ', result);
-            
-            callback(null, result);
-        })
-        .catch(err => {
-            console.log('=> an error occurred: ', err);
-            callback(err);
-        });
+        
+        client.close();
+    });
 };
+
+handler();
